2cc86a3488b7093fc6ba57113933d0e69aecb138
victor DG <v.degermain@gmail.com>
2023-01-19 15:05:28 +0100

    fix librairy import lodash full import

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  rollupOptions: {
-    external: ['lodash/debounce'],
-  },
+  }
 })
